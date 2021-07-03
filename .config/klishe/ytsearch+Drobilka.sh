39ff1278dda1744181d3b3390d19c03472f75b7c
knstntnn <59287703+knstntnn@users.noreply.github.com>
2021-07-04 01:06:56 +0800

    add "ytsearch+Drobilka.sh"
    "ytsearch+Drobilka.sh" - поиск по ютубу ч/з "ytdl-ytsearch" с переводом результатов в *.m3u

---
+++ .config/klishe/ytsearch+Drobilka.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+##   knn_knstntnn 
+##  
+## поиск по ютубу ч/з "ytdl-ytsearch" ; 
+## 
+
+q=$(echo "${@}")
+
+# $ytsNum - кол-во "первых результатов выдачи".
+ ytsNum=10
+
+youtube-dl   --get-id  -e   --flat-playlist  ytsearch"${ytsNum}":"${q}"  |  sed  "2~2{s/^/https\:\/\/youtu\.be\//}" > /tmp/yts.m3u
+
+date=$(date +%F_%H-%M)
+
+mkdir -p /$HOME/yts-$date/dev
+
+
+sed  "1~2{s/^/#EXTINF:,/}" /tmp/yts.m3u > /$HOME/yts-$date/dev/yts.m3u
+
+rm /tmp/yts.m3u
+
+echo /$HOME/yts-$date/ > /tmp/inputCat
+
+echo $HOME/yts-$date/dev/yts.m3u > /tmp/inputM3u
+
+. /usr/share/de-catenator/Drobilka_plus-IP-hdrs.sh
+
+
+
+
+
+
+
+
+
+
